811d7c849d4facab90e0f704d4dee5de6bf5d185
Hesaa23 <mahesamn23@gmail.com>
2025-11-20 00:23:33 +0700

    perbaruan ui & crud admin 3

--- resources/views/admins/layouts/navbar.blade.php
+++ resources/views/admins/layouts/navbar.blade.php
@@ -10,63 +10,18 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
+        <a class="nav-link" data-toggle="dropdown">
             <div class="user-panel d-flex align-items-center">
-              <span class="d-none d-lg-inline text-muted">{{ Auth::user()->full_name ?? 'Admin' }}</span>
+              <span class="d-none d-lg-inline text-muted">{{ Auth::guard('admin')->user()->full_name ?? 'Admin' }}</span>
                 <div class="image mr-2">
-                    <img src="{{ Auth::user()->profile_photo_url ?? asset('adminlte/dist/img/default-user.png') }}" 
-                         class="img-circle elevation-1" 
-                         alt="User Image"
-                         style="width: 30px; height: 30px;">
+                  <img src="https://i.pravatar.cc/50" class="rounded-full mr-3 shadow">
                 </div>
             </div>
         </a>
         <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
-            <span class="dropdown-item dropdown-header">{{ Auth::user()->name ?? 'Admin' }}</span>
+            <span class="dropdown-item dropdown-header">{{ Auth::guard('admin')->user()->full_name ?? 'Admin' }}</span>
             <span class="dropdown-item text-muted text-sm text-center border-bottom mb-2">{{ Auth::user()->email ?? 'admin@example.com' }}</span>
-
-            {{-- <a href="{{ route('profile.show') }}" class="dropdown-item">
-                <i class="fas fa-user-circle mr-2"></i> Profil
-            </a> --}}
-            
-            <div class="dropdown-divider"></div>
-            
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <a href="{{ route('logout') }}" 
-                   onclick="event.preventDefault(); this.closest('form').submit();"
-                   class="dropdown-item dropdown-footer text-danger">
-                    <i class="fas fa-sign-out-alt mr-1"></i> Logout
-                </a>
-            </form>
         </div>
       </li>
     </ul>
